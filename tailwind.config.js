5c71ee0d89bfc06290ad4bf50d1f724ec99d0f65
chochiniz <chochinize@gmail.com>
2021-09-18 05:41:17 +0200

    created2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily:{
+      anton:['Anton', 'sans-serif'],
+      Tenali:['Tenali Ramakrishna', 'sans-serif'],
+    },
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" }
+        }
+      },
+      animation: {
+        wiggle: "wiggle 200ms ease-in-out"
+      }
+    },
   },
   variants: {
     extend: {},
