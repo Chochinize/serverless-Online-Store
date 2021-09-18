508c17da86770b2bed9b5993d7dc5544eacf2560
chochiniz <chochinize@gmail.com>
2021-09-18 06:00:53 +0200

    another one

--- Components/Header.js
+++ Components/Header.js
@@ -1,7 +1,8 @@
 import Link from 'next/link'
 const Header = () => {
     return (
-        <header className='font-thin gap-2 sm:grid grid-cols-3  bg-gradient-to-r from-gray-100  border-b-2  justify-center    md:grid-cols-5  gap-2 text-md lg:text-2xl m-6'>
+        <header className='font-thin gap-2 sm:grid grid-cols-4  bg-gradient-to-r from-gray-100  border-b-2  justify-center    md:grid-cols-[10rem,10rem,10rem,10rem,10rem]  gap-2 text-md lg:text-2xl m-6'>
+        <div className='p-2 text-center m-6 fancy-link w-max tracking-widest'><Link href='/Shop'><a> SHOP </a></Link> </div>
         <div className='p-2 text-center m-6 fancy-link w-max tracking-widest'><Link href='/Shop'><a> SHOP </a></Link> </div>
  
     
